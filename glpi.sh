2e943d9cb1d983a33fefa1ceded6dead4e52d5dc
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-07-01 16:03:32 +0200

    Add glpi

--- glpi.sh
+++ glpi.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-DESTINATION="/var/www/glpi"
+DESTINATION="/var/www"
+APACHE_DIR="/etc/apache2/sites-available"
+APACHE_TEMPLATE="/etc/apache2/sites-available/000-default.conf"
+
+mkdir -p $DESTINATION
+
+tar -xzf ./glpi-10.0.18.tgz -C $DESTINATION
 
 cd $DESTINATION
 
-tar -xzv ./glpi-10.0.18.tgz
+touch $APACHE_DIR/001-glpi.conf || echo "Failed to create Apache configuration file, please retry as root" && exit 1
+cat $APACHE_TEMPLATE | sed "s#www.example.com#\n\tServerName glpi.local#" | sed "s#/var/www/html#$DESTINATION#" > $APACHE_DIR/001-glpi.conf
+
+a2ensite 001-glpi.conf
+sudo systemctl reload apache2
